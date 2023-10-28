949ea543a2536049df51b7906b760f7d283edfca
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-28 10:15:18 -0300

    S

--- home.test.js
+++ home.test.js
@@ -1,48 +1,22 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Home from './src/conteinerComponentes/home/home.jsx';
+import React from "react";
 
-// Mockear fetch para simular la respuesta de la API
-jest.mock('node-fetch', () => {
-  return (url) => {
-    if (url === 'URL_DE_PRUEBA') {
-      return Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            items: [
-              {
-                id: '1',
-                volumeInfo: {
-                  title: 'Libro de prueba',
-                  description: 'Descripción de prueba',
-                  imageLinks: {
-                    smallThumbnail: 'imagen.jpg',
-                  },
-                  pageCount: 100,
-                },
-              },
-            ],
-          }),
-      });
-    } else {
-      return Promise.reject(new Error('URL desconocida'));
-    }
-  };
+import isReact from "is-react";
+import Enzyme from 'enzyme';
+// import toJson from 'enzyme-to-json';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+
+// import TestRenderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Home from './src/conteinerComponentes/home/home';
+
+Enzyme.configure({ adapter: new Adapter() })
+describe("<Home />", () => {
+// Home Test with React-test-renderer
+it('renders correctly react-test-renderer', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Home />);
+  const result = renderer.getRenderOutput();
+  expect(result).toMatchSnapshot();
 });
-
-describe('Pruebas de fetchBooks en Home', () => {
-  it('debería obtener libros de la API y actualizar el estado', async () => {
-    render(<Home />);
-
-    // Asegúrate de que la función fetch se haya llamado con la URL correcta
-    expect(fetch).toHaveBeenCalledWith('URL_DE_PRUEBA');
-
-    // Espera a que se carguen los libros
-    await waitFor(() => screen.getByText('Libro de prueba'));
-
-    // Verifica que el libro se haya cargado en el estado y se esté mostrando
-    expect(screen.getByText('Libro de prueba')).toBeInTheDocument();
-    expect(screen.getByText('Descripción de prueba')).toBeInTheDocument();
-  });
-});
+})
