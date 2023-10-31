8fbec833edef10ffb75d7794bf70928e1aa4bceb
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-31 11:18:46 -0300

    movie styles

--- react/src/MovieApp/MovieApp.jsx
+++ react/src/MovieApp/MovieApp.jsx
@@ -1,8 +1,61 @@
-import "./MovieApp.module.css";
+import { useState } from "react";
+import "./MovieApp.css";
 export const MovieApp = () => {
+  const [data, setData] = useState([]);
+
+  const [movie, setMovie] = useState("");
+
+  const urlBase = "https://api.themoviedb.org/3/search/movie?query=";
+  const API_KEY = "&api_key=f69b554c57586a45f93ffd4a217dfe38";
+
+  const fetchMovie = async () => {
+    try {
+      const response = await fetch(`${urlBase}${movie}${API_KEY}`);
+      const result = await response.json();
+      setData(result.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setMovie(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    fetchMovie();
+  };
+
   return (
-    <div>
-      <h1>MovieApp</h1>
+    <div className="container">
+      <h1 className="title">MovieApp</h1>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="pelicula"
+          value={movie}
+          onChange={handleChange}
+        />
+        <button type="submit" className="search-button">
+          BUSCAR
+        </button>
+      </form>
+
+      <div className="movie-list">
+        {data?.map((mov) => {
+          return (
+            <div key={mov.id} className="movie-card">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${mov.poster_path}`}
+                alt={mov.title}
+              />
+              <h2 className="movie-card-title">{mov.title}</h2>
+              <p>{mov.overview}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 // import { AppRoute } from "./AppRoute/AppRoute";
 import { BrowserRouter } from "react-router-dom";
 import { MovieApp } from "./MovieApp/MovieApp";
