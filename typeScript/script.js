072617025364ab433b583be1f808817aefddd45e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-10 09:19:04 -0300

    function with default params

--- typeScript/script.js
+++ typeScript/script.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log(" hola Juan, si");
+let estudiasteJS = true; //boolean no va a aparecer en la compilación de js por que no soporta los tipados pero ts si
+estudiasteJS ? console.log("podes seguir aprendiendo") : console.log("TS MUST BE LEARND");
+function jugar(equipo1 = 11, equipo2 = 11) { } //void es por si no va a devolver nada, funcion con valores por defecto
--- typeScript/script.ts
+++ typeScript/script.ts
@@ -1,1 +1,7 @@
 console.log(" hola Juan, si")
+
+let estudiasteJS:boolean=true; //boolean no va a aparecer en la compilación de js por que no soporta los tipados pero ts si
+
+estudiasteJS? console.log("podes seguir aprendiendo"): console.log("TS MUST BE LEARND")
+
+function jugar(equipo1:number=11, equipo2:number=11):void {}//void es por si no va a devolver nada, funcion con valores por defecto
