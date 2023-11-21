bde94e3b6fc4732d49f65b810a74a3f76012ea2f
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-21 20:34:06 -0300

    contador

--- svelte-crud/src/lib/Counter.svelte
+++ svelte-crud/src/lib/Counter.svelte
@@ -1,10 +1,34 @@
 <script>
-  let count = 0
+  let count = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+  const decrement = () => {
+    count -= 1;
+  };
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<div>
+  <button on:click={decrement}> - </button>
+  <h3>
+    count is {count}
+  </h3>
+  <button on:click={increment}> + </button>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    justify-items: stretch;
+    align-items: center;
+  }
+  button {
+    margin: 0 5%;
+    height: 50px;
+    border: solid 1px grey;
+  }
+  button:hover {
+    border-color: rgb(70, 21, 187);
+  }
+</style>
