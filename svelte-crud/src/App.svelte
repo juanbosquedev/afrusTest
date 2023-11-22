a2e4bc964bc75a52709e201f3de9d33b4389acd2
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-21 22:51:59 -0300

    svelte form component

--- svelte-crud/src/App.svelte
+++ svelte-crud/src/App.svelte
@@ -1,13 +1,25 @@
 <script>
+  let product = {
+    id: "",
+    name: "",
+    description: "",
+    categroy: "",
+    imageURL: "",
+  };
 </script>
 
 <main>
   <form>
     <input type="text" placeholder="product name" id="product-name" />
-    <textarea id="product-description" rows=3 placeholder="product-description"></textarea>
+    <textarea
+      id="product-description"
+      rows="3"
+      placeholder="product-description"
+    />
     <input type="url" placeholder="product name" id="product-image-url" />
-     <button>save Product</button>
-</form>
+    <select  id="category"><option value=""></option></select>
+    <button>save Product</button>
+  </form>
 </main>
 
 <style></style>
