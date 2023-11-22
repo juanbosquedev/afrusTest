319c4e8bccc60e40eea86dc770352ae7e659c029
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-21 21:10:18 -0300

    componente app

--- svelte-crud/src/App.svelte
+++ svelte-crud/src/App.svelte
@@ -1,7 +1,13 @@
 <script>
-
 </script>
+
 <main>
-    <h1>hola</h1>
+  <form>
+    <input type="text" placeholder="product name" id="product-name" />
+    <textarea id="product-description" rows=3 placeholder="product-description"></textarea>
+    <input type="url" placeholder="product name" id="product-image-url" />
+     <button>save Product</button>
+</form>
 </main>
+
 <style></style>
