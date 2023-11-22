4868e9f3ce231ff4b363f1f3d28bb507b8f68a28
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-22 20:11:12 -0300

    boostrap style

--- svelte-crud/src/App.svelte
+++ svelte-crud/src/App.svelte
@@ -6,20 +6,46 @@
     categroy: "",
     imageURL: "",
   };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 </script>
 
 <main>
-  <form>
-    <input type="text" placeholder="product name" id="product-name" />
+  <form on:submit={onSubmitHandler}>
+    <input
+      bind:value={product.name}
+      type="text"
+      placeholder="product name"
+      id="product-name"
+      class="form-control"
+    />
     <textarea
+      bind:value={product.description}
       id="product-description"
       rows="3"
       placeholder="product-description"
+      class="form-control"
     />
-    <input type="url" placeholder="product name" id="product-image-url" />
-    <select  id="category"><option value=""></option></select>
+    <input
+      bind:value={product.imageURL}
+      type="url"
+      placeholder="product name"
+      id="product-image-url"
+      class="form-control"
+    />
+    <select bind:value={product.categroy} id="category" class="form-control">
+      <option value="laptos" placeholder="product name" id="product-image-url"
+        >laptos</option
+      >
+      <option value="peripherials">periferials</option>
+      <option value="servers">server</option>
+    </select>
     <button>save Product</button>
   </form>
+  {product.name}
 </main>
 
 <style></style>
