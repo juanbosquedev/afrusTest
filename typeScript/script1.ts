e5f22fe2bd985aa08064437f416adfc3075bc7c3
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-24 09:39:13 -0300

    default parameters

--- typeScript/script1.ts
+++ typeScript/script1.ts
@@ -2,3 +2,4 @@ console.log(" aprendi mucho en esta sección")
 
 // In TS there are optional and default / paramaters
 
+//OPTIONAL PARAMETERS num?:string, add(num1:numbar, num2:number=2) this is a way to pass an optional parameters in case you don't pass one
