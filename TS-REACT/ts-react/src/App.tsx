544b239919a49c60596974fe638aab8cec11dfc0
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-27 11:29:45 -0300

    contenido hover

--- TS-REACT/ts-react/src/App.tsx
+++ TS-REACT/ts-react/src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import { Greeting } from './components/Greeting'
 
 function App() {
 
   return (
     <>
-     
+     <Greeting name={}></Greeting>
     </>
   )
 }
---
+++ TS-REACT/ts-react/src/components/Greeting.tsx
@@ -0,0 +1,6 @@
+
+export const Greeting = ({name}) => {
+  return (
+    <div>Greeting {name}</div>
+  )
+}
