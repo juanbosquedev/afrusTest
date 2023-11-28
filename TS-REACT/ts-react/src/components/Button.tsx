abe1cdc3860b395c8f1a52db0231e63118434cf3
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-11-28 08:45:57 -0300

    handle click

--- TS-REACT/ts-react/src/components/Button.tsx
+++ TS-REACT/ts-react/src/components/Button.tsx
@@ -1,6 +1,14 @@
+//typado de eventos, sino devuelve nada el evento, pasamos un arrow function que devuelve oun void
+//otra variante de typado de evento es cuando necesitamos pasar el Evento en si atraves del handlerClick
+//el tipo del evento lo definimos con React type; que es un evento del mouse
+interface ButtonProps{
+    // handleClick:()=>void,
+    handleClick:(event:React.MouseEvent<HTMLButtonElement>, id:number)=>void,
 
-export const Button = () => {
+}
+
+export const Button = (props:ButtonProps) => {
   return (
-    <div>Button</div>
+    <div onClick={(event)=>props.handleClick(event,1)}>Button</div>
   )
 }
